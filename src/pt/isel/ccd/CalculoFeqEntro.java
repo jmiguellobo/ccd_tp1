891128388741d7c1692e1f449fdeff5bac1db83d
Jorge Lobo <jmnsl92@gmail.com>
2016-12-01 16:10:16 +0000

    Exercicios 1 e 2 completos 1

--- src/pt/isel/ccd/CalculoFeqEntro.java
+++ src/pt/isel/ccd/CalculoFeqEntro.java
@@ -2,29 +2,40 @@ package pt.isel.ccd;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class CalculoFeqEntro {
 
-	
-	private final static String ficheiro = "lena.zip";
+	private final static String ficheiro = "alice29.txt";
 	private final static String path = "/Users/Mig/Dropbox/Mestrado Eng. Informática/1ºAno - 1ºSemestre/Compressão e Codificação de Dados/ficheiros/"
 			+ ficheiro;
 	private final static Util newUtil = new Util();
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		File file = new File(path);
 
 		HashMap<String, Double> pMap = readFile(file);
-		
-		
+
+		// Gerar ficheiro
+
+		try {
+			writeFile(file, pMap);
+
+		} catch (IOException e) { // TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
-	
 	public static HashMap<String, Double> readFile(File file) {
 		FileInputStream fileInputStream;
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
@@ -47,10 +58,10 @@ public class CalculoFeqEntro {
 
 			}
 			double entropia = calcH(bFile, map, pMap);
-			
-			Util.writeLine(pMap, path.substring(0, path.length()-4)+path.substring(path.length()-3)+".csv"); 
-					//path.split("\\.")[0]+"_"+path.split("\\.")[1]+"csv");
-			
+
+			Util.writeLine(pMap, path.substring(0, path.length() - 4) + path.substring(path.length() - 3) + ".csv");
+			// path.split("\\.")[0]+"_"+path.split("\\.")[1]+"csv");
+
 			System.out.println("Entropia: " + entropia);
 
 			System.out.println("Done " + bFile.length);
@@ -59,12 +70,12 @@ public class CalculoFeqEntro {
 		}
 		return pMap;
 	}
-	
-	
-	private static double calcH(byte[] bFile, HashMap<String, Integer> map, HashMap<String, Double> pMap) throws IOException {
+
+	private static double calcH(byte[] bFile, HashMap<String, Integer> map, HashMap<String, Double> pMap)
+			throws IOException {
 
 		double H = 0;
-		
+
 		for (int h = 0; h <= 255; h++) {
 			Integer simblo = map.get(String.valueOf(h));
 			if (simblo != null) {
@@ -74,10 +85,7 @@ public class CalculoFeqEntro {
 				System.out.println(h + "," + Px + "\n");
 
 				pMap.put(String.valueOf(h), Px);
-				
-			
-				
-				
+
 				System.out.println("index = " + h + " prob= " + pMap.get(String.valueOf(h)));
 
 			} else {
@@ -85,6 +93,38 @@ public class CalculoFeqEntro {
 			}
 		}
 		return H;
+	}
+
+	private static void writeFile(File file, HashMap<String, Double> pMap)
+			throws FileNotFoundException, IOException {
+		StringBuilder builder = new StringBuilder();
+		byte[] bFile = new byte[(int) file.length()];
+		for (int i = 0; i < bFile.length; i++) {
+
+			builder.append((char) (getSym(pMap)));
+
+		}
+
+		FileOutputStream out = new FileOutputStream(new File(
+				path.substring(0, path.length() - 4) +"New"+ path.substring(path.length() - 4)));
+		out.write(builder.toString().getBytes());
+		out.close();
+	}
+
+	private static int getSym(HashMap<String, Double> pMap) {
+		Random rand = new Random();
+		Double valRand = rand.nextDouble();
+		Double sumEntry = 0.0;
+
+		for (Map.Entry<String, Double> entry : pMap.entrySet()) {
+			sumEntry += entry.getValue();
+			if (sumEntry >= valRand) {
+				return Integer.valueOf(entry.getKey());
+			}
+
+		}
+
+		return 0;
 	}
 
 }
